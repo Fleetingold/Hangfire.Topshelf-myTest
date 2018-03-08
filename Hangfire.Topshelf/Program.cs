01062af7bcd014a12c9610c6dd7124b8da0fbe63
fleetingold <1161851523@qq.com>
2018-03-08 11:28:47 +0800

    添加项目文件。

---
+++ Hangfire.Topshelf/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Hangfire.Topshelf.Core;
+using Serilog;
+using Topshelf;
+
+namespace Hangfire.Topshelf
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Verbose()
+				.WriteTo.LiterateConsole()
+				.WriteTo.RollingFile("logs\\log-{Date}.txt")
+				.CreateLogger();
+
+			return (int)HostFactory.Run(x =>
+			{
+				x.RunAsLocalSystem();
+
+				x.SetServiceName(HangfireSettings.Instance.ServiceName);
+				x.SetDisplayName(HangfireSettings.Instance.ServiceDisplayName);
+				x.SetDescription(HangfireSettings.Instance.ServiceDescription);
+
+				x.UseOwin(baseAddress: HangfireSettings.Instance.ServiceAddress);
+
+				x.SetStartTimeout(TimeSpan.FromMinutes(5));
+				//https://github.com/Topshelf/Topshelf/issues/165
+				x.SetStopTimeout(TimeSpan.FromMinutes(35));
+
+				x.EnableServiceRecovery(r => { r.RestartService(1); });
+			});
+		}
+	}
+}
---
+++ cluster/HF.Samples.APIs/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.Swagger.Model;
+using Hangfire;
+
+namespace HF.Samples.APIs
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+			if (env.IsEnvironment("Development"))
+			{
+				// This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+				builder.AddApplicationInsightsSettings(developerMode: true);
+			}
+
+			builder.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add framework services.
+			services.AddApplicationInsightsTelemetry(Configuration);
+
+			services.AddMvc();
+
+            // var connectionString = Configuration.GetConnectionString("hangfire.redis");
+            // GlobalConfiguration.Configuration.UseRedisStorage(connectionString);
+            // var connectionString = "server=192.168.1.250;database=Hangfire;uid=sa;pwd=abc-123;Pooling=False;";
+             GlobalConfiguration.Configuration.UseSqlServerStorage("server=192.168.1.250;database=Hangfire;uid=sa;pwd=abc-123;Pooling=False;");
+
+            services.AddSwaggerGen();
+
+			var xmlPath = GetXmlCommentsPath();
+
+			services.ConfigureSwaggerGen(options =>
+			{
+				options.SingleApiVersion(new Info
+				{
+					Version = "v1",
+					Title = "Hangfire Samples APIs",
+					Description = "The unified entry for hangfire invocation to add background job to queues.",
+					TermsOfService = "None",
+					Contact = new Contact { Name = "icsharp", Url = "https://github.com/icsharp/Hangfire.Topshelf" }
+				});
+
+				options.IncludeXmlComments(xmlPath);
+
+				//options.DescribeAllEnumsAsStrings();
+			});
+		}
+		private string GetXmlCommentsPath()
+		{
+			var app = PlatformServices.Default.Application;
+			return Path.Combine(app.ApplicationBasePath, "HF.Samples.APIs.xml");
+		}
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			app.UseApplicationInsightsRequestTelemetry();
+
+			app.UseApplicationInsightsExceptionTelemetry();
+
+			app.UseMvc();
+
+			app.UseSwagger();
+
+			app.UseSwaggerUi();
+		}
+	}
+}
