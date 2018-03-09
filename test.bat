2a616c9b30816973fe21e05d691eb9b573046d32
fleetingold <1161851523@qq.com>
2018-03-09 14:11:51 +0800

    20180309

--- test.bat
+++ test.bat
@@ -2,11 +2,11 @@
 
 set dir="cluster"
 
-start dotnet run -p %dir%\HF.Samples.ServerNode nodeA -q order -w 100
+rem start dotnet run -p %dir%\HF.Samples.ServerNode nodeA -q order -w 100
 
-start dotnet run -p %dir%\HF.Samples.ServerNode nodeB -q storage -w 100
+rem start dotnet run -p %dir%\HF.Samples.ServerNode nodeB -q storage -w 100
 
-rem start cmd /k "cd %dir%\HF.Samples.Console && dotnet run"
+start cmd /k "cd %dir%\HF.Samples.Console && dotnet run"
 
 rem start cmd /k "cd %dir%\HF.Samples.APIs && dotnet run"
 
